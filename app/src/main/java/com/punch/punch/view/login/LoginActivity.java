0b0377dd7aa0fb6e1c710265664aeec52d493563
lms <lms8147@gmail.com>
2018-06-23 19:53:52 +0900

    LoginActivity 패키지 이동

--- app/src/main/java/com/punch/punch/view/login/LoginActivity.java
+++ app/src/main/java/com/punch/punch/view/login/LoginActivity.java
@@ -1,7 +1,9 @@
-package com.punch.punch;
+package com.punch.punch.view.login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.punch.punch.R;
 
 public class LoginActivity extends AppCompatActivity {
 
